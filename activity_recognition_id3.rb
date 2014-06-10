5da6c46617f3855fc8c0863b679e07b23085da75
xautjzd <xautjzd@gmail.com>
2014-06-10 15:37:12 +0800

    add id3 algorithm to activity recognition

---
+++ activity_recognition_id3.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+################################################################
+# Refer: https://github.com/igrigorik/decisiontree
+# Thanks very much to Igrigorik for your decisiontree
+################################################################
+
+require_relative 'orm'
+require_relative 'id3_tree'
+
+attributes = [
+	"X_Average", 
+	"Y_Average",
+	"Z_Average",
+	"X_Deviation",
+	"Y_Deviation",
+	"Z_Deviation",
+	"XY_Correlation",
+	"YZ_Correlation",
+	"XZ_Correlation"
+]
+
+samples = Attribute.take(432)
+inputs = Attribute.last(100)
+
+training = []
+
+samples.each do |sample|
+	training << [sample.X_Average, sample.Y_Average, sample.Z_Average, sample.X_Deviation, sample.Y_Deviation, sample.Z_Deviation, sample.XY_Correlation, sample.YZ_Correlation, sample.XZ_Correlation, sample.Action]
+end
+
+dec_tree = DecisionTree::ID3Tree.new(
+	attributes,
+	training, 
+	"上楼",
+	X_Average: :continuous,
+	Y_Average: :continuous,
+	Z_Average: :continuous,
+	X_Deviation: :continuous,
+	Y_Deviation: :continuous,
+	Z_Deviation: :continuous,
+	XY_Correlation: :continuous,
+	YZ_Correlation: :continuous,
+	XZ_Correlation: :continuous
+)
+
+dec_tree.train
+
+counter = 0
+inputs.each do |input|
+	test = [input.X_Average, input.Y_Average, input.Z_Average, input.X_Deviation, input.Y_Deviation, input.Z_Deviation, input.XY_Correlation, input.YZ_Correlation, input.XZ_Correlation, input.Action]
+	decision = dec_tree.predict(test)
+
+	if decision == test.last
+		counter += 1
+	end
+end
+
+print "ID3 algorithm recognition accuracy: ", counter, "%\n"
